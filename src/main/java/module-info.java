d120c4a660cef8fcae790da1f8739c38ebd72619
agattt <a.modzelewsk12@student.uw.edu.pl>
2022-06-10 23:10:01 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.sudoku {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.sudoku to javafx.fxml;
+    exports com.example.sudoku;
+}
